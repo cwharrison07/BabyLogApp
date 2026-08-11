a5576e9540beaa68ff85ad58ddf7c655140dc2be
cwharrison07 <chase.w.harrison07@gmail.com>
2026-08-10 22:32:00 -0400

    Adds Stats

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Xml.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
@@ -9,11 +11,17 @@ app.UseStaticFiles();
 
 app.MapGet("/newLog", (HttpContext context) =>
 {
-    if (context.Session.GetString("OliverSleep") == null)
-    {
-        context.Session.SetString("OliverSleep", "Fell Asleep");
-        context.Session.SetString("IslaSleep", "Fell Asleep");
-    }
+    if (context.Session.GetString("OliverSleep") == null) context.Session.SetString("OliverSleep", "Fell Asleep");
+    if (context.Session.GetString("IslaSleep") == null) context.Session.SetString("IslaSleep", "Fell Asleep");
+
+    if (context.Session.GetInt32("OliverNapCount") == null) context.Session.SetInt32("OliverNapCount", 0);
+    if (context.Session.GetInt32("IslaNapCount") == null) context.Session.SetInt32("IslaNapCount", 0);
+    if (context.Session.GetInt32("OliverAmountEaten") == null) context.Session.SetInt32("OliverAmountEaten", 0);
+    if (context.Session.GetInt32("IslaAmountEaten") == null) context.Session.SetInt32("IslaAmountEaten", 0);
+    if (context.Session.GetInt32("OliverPoopCount") == null) context.Session.SetInt32("OliverPoopCount", 0);
+    if (context.Session.GetInt32("IslaPoopCount") == null) context.Session.SetInt32("IslaPoopCount", 0);
+    if (context.Session.GetInt32("OliverNapTime") == null) context.Session.SetInt32("OliverNapTime", 0);
+    if (context.Session.GetInt32("IslaNapTime") == null) context.Session.SetInt32("IslaNapTime", 0);
 
     return Results.Content(
     $$"""
@@ -71,6 +79,23 @@ app.MapGet("/newLog", (HttpContext context) =>
 
                 <div id="eventLogIsla" class="event-log"></div>
             </div>
+
+            <div class="stats">
+                <label id="oliver-stat-label">Oliver</label>
+                <label id="isla-stat-label">Isla</label>
+                <label class="stat-title"># of Naps:</label>
+                <label id="oliver-nap-count">{{context.Session.GetInt32("OliverNapCount")}}</label>
+                <label id="isla-nap-count">{{context.Session.GetInt32("IslaNapCount")}}</label>
+                <label class="stat-title">Time Asleep:</label>
+                <label id="oliver-nap-time">{{context.Session.GetInt32("OliverNapTime")}}</label>
+                <label id="isla-nap-time">{{context.Session.GetInt32("IslaNapTime")}}</label>
+                <label class="stat-title">Amount Eaten:</label>
+                <label id="oliver-amount-eaten">{{context.Session.GetInt32("OliverAmountEaten")}}</label>
+                <label id="isla-amount-eaten">{{context.Session.GetInt32("IslaAmountEaten")}}</label>
+                <label class="stat-title"># of Poops:</label>
+                <label id="oliver-poop-count">{{context.Session.GetInt32("OliverPoopCount")}}</label>
+                <label id="isla-poop-count">{{context.Session.GetInt32("IslaPoopCount")}}</label>
+            </div>
         <script>
             let timeSlider = document.getElementById("toggleTime");
 
@@ -120,13 +145,26 @@ app.MapGet("/newLog", (HttpContext context) =>
                     }
                     timeInput.value = "";
                 }
+                let name = "Oliver";
+                if (slider.value == 1) name = "Isla";
+
                 let eventLog = document.createElement("label");
                 if (input.id === "sleep") {
-                    let name = "Oliver";
-                    if (slider.value == 1) name = "Isla";
+                    
 
                     if (input.textContent === "Fell Asleep") {
-                        fetch(`/setSleep?name=${name}&status=Woke%20Up`);
+                        fetch(`/setSleep?name=${name}&status=Woke%20Up&time=${time}`)
+                            .then(response => response.json())
+                            .then(data => {
+                                timeAsleep = minutesToHHMM(data.napTime);
+                                if (data.name === "Oliver") {
+                                    document.getElementById("oliver-nap-count").textContent = data.napCount;
+                                    document.getElementById("oliver-nap-time").textContent = timeAsleep;
+                                } else {
+                                    document.getElementById("isla-nap-count").textContent = data.napCount;
+                                    document.getElementById("isla-nap-time").textContent = timeAsleep;
+                                }
+                            });
                         input.textContent = "Woke Up";
 
                         if (name === "Oliver") oliverSleep = "Woke Up";
@@ -135,7 +173,18 @@ app.MapGet("/newLog", (HttpContext context) =>
                         eventLog.textContent = time + ": Fell Asleep";
                         eventLogHTML.appendChild(eventLog);
                     } else {
-                        fetch(`/setSleep?name=${name}&status=Fell%20Asleep`);
+                        fetch(`/setSleep?name=${name}&status=Fell%20Asleep&time=${time}`)
+                            .then(response => response.json())
+                            .then(data => {
+                                timeAsleep = minutesToHHMM(data.napTime);
+                                if (data.name === "Oliver") {
+                                    document.getElementById("oliver-nap-count").textContent = data.napCount;
+                                    document.getElementById("oliver-nap-time").textContent = timeAsleep;
+                                } else {
+                                    document.getElementById("isla-nap-count").textContent = data.napCount;
+                                    document.getElementById("isla-nap-time").textContent = timeAsleep;
+                                }
+                            });
                         input.textContent = "Fell Asleep";
 
                         if (name === "Oliver") oliverSleep = "Fell Asleep";
@@ -145,6 +194,15 @@ app.MapGet("/newLog", (HttpContext context) =>
                         eventLogHTML.appendChild(eventLog);
                     }
                 } else if (input.id === "eat") {
+                    fetch(`/updateIntake?name=${name}&amount=${input.value.substring(0, 1)}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.name === "Oliver") {
+                                document.getElementById("oliver-amount-eaten").textContent = data.amountEaten;
+                            } else {
+                                document.getElementById("isla-amount-eaten").textContent = data.amountEaten;
+                            }
+                        });
                     eventLog.textContent = time + ": Ate " + input.value;
                     input.value = "";
                     eventLogHTML.appendChild(eventLog);
@@ -152,9 +210,25 @@ app.MapGet("/newLog", (HttpContext context) =>
                     eventLog.textContent = time + ": Changed Diaper (Pee)";
                     eventLogHTML.appendChild(eventLog);
                 } else {
+                    fetch(`/updateOutput?name=${name}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.name === "Oliver") {
+                                document.getElementById("oliver-poop-count").textContent = data.poopCount;
+                            } else {
+                                document.getElementById("isla-poop-count").textContent = data.poopCount;
+                            }
+                        });
                     eventLog.textContent = time + ": Changed Diaper (Poop)";
                     eventLogHTML.appendChild(eventLog); 
                 }
+            }
+
+            function minutesToHHMM(totalMinutes) {
+                let hours = Math.floor(totalMinutes / 60);
+                let minutes = totalMinutes % 60;
+
+                return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`;
             }
         </script>
     </body>
@@ -163,7 +237,7 @@ app.MapGet("/newLog", (HttpContext context) =>
     "text/html");
 });
 
-app.MapGet("/setSleep", (HttpContext context, string name, string status) =>
+app.MapGet("/setSleep", (HttpContext context, string name, string status, string time) =>
 {
     if (name == "Oliver")
     {
@@ -174,7 +248,57 @@ app.MapGet("/setSleep", (HttpContext context, string name, string status) =>
         context.Session.SetString("IslaSleep", status);
     }
 
-    return Results.Ok();
+    int hour = int.Parse(time.Substring(0, time.IndexOf(":"))) % 12;
+    int min = int.Parse(time.Substring(time.IndexOf(":") + 1, 2));
+    int timeInMin = 60 * hour + min;
+
+    if (time.Contains("PM", StringComparison.OrdinalIgnoreCase)) timeInMin += 720;
+
+    if (status == "Woke Up")
+    {
+        
+        context.Session.SetInt32($"{name}NapCount", (int)context.Session.GetInt32($"{name}NapCount")! + 1);
+        context.Session.SetInt32($"{name}StartSleep", timeInMin);
+    } else
+    {
+        context.Session.SetInt32($"{name}NapTime", (int)context.Session.GetInt32($"{name}NapTime")! + timeInMin - (int)context.Session.GetInt32($"{name}StartSleep")!);
+    }
+
+    return Results.Json(new
+    {
+        name = name,
+        napCount = context.Session.GetInt32($"{name}NapCount"),
+        napTime = context.Session.GetInt32($"{name}NapTime")
+    });
 });
+
+app.MapGet("/updateIntake", (HttpContext context, string name, int amount) =>
+{
+    int current = context.Session.GetInt32($"{name}AmountEaten") ?? 0;
+    int newAmount = current + amount;
+
+    context.Session.SetInt32($"{name}AmountEaten", newAmount);
+
+    return Results.Json(new
+    {
+        name = name,
+        amountEaten = newAmount
+    });
+});
+
+app.MapGet("/updateOutput", (HttpContext context, string name) =>
+{
+    int current = context.Session.GetInt32($"{name}PoopCount") ?? 0;
+    int newCount = current + 1;
+
+    context.Session.SetInt32($"{name}PoopCount", newCount);
+
+    return Results.Json(new
+    {
+        name = name,
+        poopCount = newCount
+    });
+});
+
 
 app.Run();
